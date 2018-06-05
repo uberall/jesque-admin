08513deeab168fa1bbc00d6fab1decfdd034b6cc
Florian Langenhahn <florian.langenhahn@uberall.com>
2018-06-06 00:31:31 +0200

    remove dead code
    queueInfoDao.getQueueInfo always gives back a QueueInfo object so the null
    check is useless and can be removed

--- grails-app/controllers/grails/plugins/jesque/admin/JesqueAdminQueueController.groovy
+++ grails-app/controllers/grails/plugins/jesque/admin/JesqueAdminQueueController.groovy
@@ -1,17 +1,10 @@
 package grails.plugins.jesque.admin
 
-import org.springframework.http.HttpStatus
-
 class JesqueAdminQueueController extends AbstractJesqueAdminController {
 
     def details(String name) {
         def queue = queueInfoDao.getQueueInfo(name, params.getLong('offset', 0), params.getLong('max', 25))
-        if (!queue) {
-            response.status = HttpStatus.NOT_FOUND.value()
-            jsonRender([status: HttpStatus.NOT_FOUND])
-        } else {
-            jsonRender([queue: queue])
-        }
+        jsonRender([queue: queue])
     }
 
     def remove(String name) {

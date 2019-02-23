ebdcaa8a85b1534ecf625e19c4a701eecbf4e38a
Florian Langenhahn <florian.langenhahn@uberall.com>
2019-02-24 00:56:26 +0100

    don't show worker state in list when details window is toggled

--- frontend/scripts/components/workers/list-row.js
+++ frontend/scripts/components/workers/list-row.js
@@ -1,12 +1,12 @@
 import BaseComponent from "../base-component";
 import React from "react";
 import FromNow from "../common/from-now";
-const cx = require('classnames')
+const cx = require('classnames');
 
 export default class WorkerListRow extends BaseComponent {
 
   render() {
-    const {worker, selectable, onClick, selected, onDelete} = this.props
+    const {worker, selectable, onClick, selected, onDelete} = this.props;
     const {host, pid, state, status, queues} = worker;
     let job = "";
     let since = "";
@@ -18,15 +18,15 @@ export default class WorkerListRow extends BaseComponent {
     const baseKey = `${host}-${pid}`;
     const cols = [
       (<td key={`${baseKey}-host`}>{host}</td>),
-      (<td key={`${baseKey}-pid`}>{pid}</td>),
-      (<td key={`${baseKey}-state`}>{state}</td>),
+      (<td key={`${baseKey}-pid`}>{pid}</td>)
     ];
     if (!selected) {
+      cols.push(<td key={`${baseKey}-state`}>{state}</td>);
       cols.push(<td key={`${baseKey}-job`}>{job}</td>);
       cols.push(<td key={`${baseKey}-since`}>{since}</td>);
       cols.push(<td key={`${baseKey}-queue`}><span title={queues.join(",")}>{queues.length}</span></td>);
       cols.push(<td key={`${baseKey}-actions`}>
-        <button className="btn btn-danger btn-xs pull-right" onClick={onDelete}><i className="fa fa-trash"></i></button>
+        <button className="btn btn-danger btn-xs pull-right" onClick={onDelete}><i className="fa fa-trash"/></button>
       </td>);
     }
 
